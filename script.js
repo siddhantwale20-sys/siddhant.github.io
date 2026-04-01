b84e3ca45cc6a5cfee8c661d12f454368f882d8b
siddhantwale20-sys <siddhantwale20@gmail.com>
2026-04-01 12:15:56 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple welcome alert
+window.onload = () => {
+    console.log("Smart Parking Website Loaded 🚗");
+};
+
+// Button interaction example
+document.addEventListener("click", function() {
+    console.log("User clicked somewhere");
+});
